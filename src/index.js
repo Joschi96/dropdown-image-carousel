8e21ef3bafbba9f4d33b102bf5855f743d4bef24
Joschi96 <117238956+Joschi96@users.noreply.github.com>
2025-03-15 13:08:24 +0100

    add click event for first button, not working properly

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import 'normalize.css';
 import './styles.css';
+
+// collect all the buttons
+const buttonCl = document.querySelector('.click');
+const buttonHover = document.querySelector('.hover');
+const buttonOutside = document.querySelector('.click-outside');
+
+// collect all dropdown menus
+const dropdownCl = document.querySelector('.dropdown-menu-click');
+const dropdownHover = document.querySelector('.dropdown-menu-hover');
+const dropdownOutside = document.querySelector('.dropdown-menu-click-outside');
+
+function handleButtonClick() {
+  dropdownCl.classList.toggle('show');
+}
+
+buttonCl.addEventListener('click', handleButtonClick);
